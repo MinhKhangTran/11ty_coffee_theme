9640de187b1765c1342f1e1d90139909cb342517
MinhKhangTran <minhkhang.tran@gmx.de>
2022-04-08 06:01:35 +0200

    🐛 pass through admin

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/images");
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/js");
+  eleventyConfig.addPassthroughCopy("./src/admin");
   eleventyConfig.addWatchTarget("./src/scss/");
 
   return {
